bac487e84162379e9c661da92f2f6cc29971ec5f
root <kikivega2002@gmail.com>
2023-10-19 11:38:05 -0400

    primer componente

---
+++ src/Component_01.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Component_01.css';
+
+function Component_01() {
+  return (
+    <>
+      <div className="Cuadro_01">Component_01</div>
+    </>
+  );
+}
+
+export default Component_01;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Component_01 from './Component_01.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Component_01 />
+  </React.StrictMode>,
+)
